5cc03d2d5426399cd99fb09883a3bd30be85e712
ffuhu <80032043+ffuhu@users.noreply.github.com>
2022-01-20 17:53:58 +0100

    Launching scripts for 10D - Paths corrected for Colab (non-colab also included)

--- launchers/train_1510ch_FT_UNet_BAR.sh
+++ launchers/train_1510ch_FT_UNet_BAR.sh
@@ -8,8 +8,8 @@
 #VAL_DIR_FT="${HOME}/Scratch/nus/00_data/data_Florian/Dataset_Multiview/denoised/cropped_110_110_1024_1024/denoised_cropped_down_512px/good/manually_selected_5-2/stacks/"
 
 # colab
-TRAIN_DIR_SG="./data/00010--dsmult3-xflip-yflip-rot-clean_and_blurry-twoheads-3_128_128-low_shot_mvds-batch16-color-translation-cutout/out/"
-VAL_DIR_SG="./data/00010--dsmult3-xflip-yflip-rot-clean_and_blurry-twoheads-3_128_128-low_shot_mvds-batch16-color-translation-cutout/out/"
+TRAIN_DIR_SG="../data/00010--dsmult3-xflip-yflip-rot-clean_and_blurry-twoheads-3_128_128-low_shot_mvds-batch16-color-translation-cutout/out/"
+VAL_DIR_SG="../data/00010--dsmult3-xflip-yflip-rot-clean_and_blurry-twoheads-3_128_128-low_shot_mvds-batch16-color-translation-cutout/out/"
 TRAIN_DIR_FT="../data/Dataset_Multiview_denoised_cropped_110_110_1024_1024_denoised_cropped_down_512px_good_manually_selected_5-2_stacks/"
 VAL_DIR_FT="../data/Dataset_Multiview_denoised_cropped_110_110_1024_1024_denoised_cropped_down_512px_good_manually_selected_5-2_stacks/"
 
